5d0bb2e0497b3e73a5d92f65a92ab642a09239fe
dbasdanis <dbasdanis97@gmail.com>
2023-05-20 15:54:15 +0200

    feat: add rendering in the app

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask import request
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    user_agent = request.headers.get('User-Agent')
-    return '<p>Your browser is %s</p>' %user_agent 
+    # user_agent = request.headers.get('User-Agent')
+    # return '<p>Your browser is %s</p>' %user_agent 
+    return render_template('index.html')
 
 @app.route('/user/<name>')
 def user(name):
