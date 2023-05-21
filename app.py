79916f6517441e1965f3bf0563801335317d9281
dbasdanis <dbasdanis97@gmail.com>
2023-05-21 15:43:16 +0200

    feat: add datetime on index

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
+from datetime import datetime
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
 
 app = Flask(__name__)
 
 bootstrap = Bootstrap(app)
-
+moment = Moment(app)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', currect_time=datetime.utcnow())
 
 
 @app.route('/user/<name>')
